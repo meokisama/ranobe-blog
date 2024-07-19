b8ad8b8005030eb102f2f37f50c1efecc6bba33f
meokisama <hoangdinhsang123@gmail.com>
2024-07-19 11:15:28 +0700

    responsive for hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -12,21 +12,6 @@ export default function Hero() {
       <div
         className={`w-full h-full absolute bg-[length:3vw_3vw] lg:bg-[length:1.5vw_1.5vw] -z-10 ${style.caro}`}
       />
-      <div className="w-full absolute bottom-0 z-50 select-none pointer-events-none">
-        <motion.div
-          initial={{ scale: 1.2 }}
-          animate={{ scale: 1 }}
-          transition={{ duration: 1, delay: 6.7 }}
-        >
-          <Image
-            src="/imouza.png"
-            alt="imouza banner"
-            width={1500}
-            height={1500}
-            className="w-[90vw] sm:w-[500px] lg:w-[40vw] h-auto block my-0 mx-auto"
-          />
-        </motion.div>
-      </div>
       <div>
         <div className="mt-20 mr-10 -rotate-12 text-[#e7b088b8]">
           <motion.h1
@@ -48,16 +33,13 @@ export default function Hero() {
         </div>
         <div className="w-full flex flex-col-reverse lg:flex-row lg:gap-[35vw]">
           <div className="w-1/2"></div>
-          <div className="w-1/2 pr-[4vw] font-grotesque rotate-6">
+          <div className="w-full px-10 lg:w-1/2 lg:pr-[4vw] font-grotesque lg:rotate-6">
             <motion.div
               initial={{ opacity: 0, y: "-50px" }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 6.7 }}
             >
-              <p
-                className="lg:text-[1.3vw] text-justify leading-tight -translate-y-[1vw]"
-                style={{ direction: "rtl" }}
-              >
+              <p className="lg:text-[1.3vw] text-justify leading-tight mt-16 md:mt-32 lg:mt-0 lg:-translate-y-[1vw]">
                 Có bao giờ bạn tự nghĩ những cuốn light novel ngoài kia có thể
                 ẩn chứa những bí mật kín đáo hơn những gì mắt thấy? Đã bao giờ
                 bạn tự hỏi liệu mình đã thật sự thấu hiểu hết những thông điệp
@@ -66,7 +48,7 @@ export default function Hero() {
                 những bí mật chờ đợi bạn khám phá. Và đây là một blog sẽ đi cùng
                 bạn trong hành trình đó
               </p>
-              <div className="flex flex-row float-right gap-4">
+              <div className="flex flex-row justify-center lg:float-right gap-1 lg:gap-4 mt-4">
                 <Link
                   href="https://facebook.com/NaviRanobe"
                   target="_blank"
@@ -85,6 +67,21 @@ export default function Hero() {
             </motion.div>
           </div>
         </div>
+      </div>
+      <div className="w-full lg:absolute bottom-0 z-50 select-none pointer-events-none">
+        <motion.div
+          initial={{ scale: 1.2 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 1, delay: 6.7 }}
+        >
+          <Image
+            src="/imouza.png"
+            alt="imouza banner"
+            width={1500}
+            height={1500}
+            className="w-[70vw] sm:w-[500px] lg:w-[40vw] h-auto block my-0 mx-auto"
+          />
+        </motion.div>
       </div>
     </div>
   );
