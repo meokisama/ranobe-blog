81c9182871316485c6ec2e91f3156479e8146bcf
meokisama <hoangdinhsang123@gmail.com>
2024-07-30 22:50:55 +0700

    add bento grid

--- components/home/bento.tsx
+++ components/home/bento.tsx
@@ -1,24 +1,172 @@
+import Image from "next/image";
+
 export default function Bento() {
   return (
     <div className="flex justify-center items-center mt-20">
-      <div className="grid h-screen w-[95%] xl:w-[80%] grid-cols-5 gap-2 xl:gap-4">
-        <div className="col-span-2 bento-item">Paradise Found</div>
-        <div className="col-span-2 bento-item">History Unveiled</div>
-        <div className="bento-item">Marrakesh Magic</div>
-        <div className="bento-item">Cappadocia, Turkey</div>
-        <div className="col-span-3 row-span-2">
-          <div className="flex h-full w-full flex-col items-center justify-center gap-6 rounded-xl lg:rounded-3xl border dark:border-white/[0.2] px-12 text-center overflow-hidden">
-            <h1 className="text-6xl font-bold">Trsuxhxb</h1>
-            <h3 className="text-3xl font-semibold">dfbgggtbhrhn</h3>
-          </div>
+      <div className="grid min-h-screen w-[95%] xl:w-[80%] grid-cols-5 gap-2 xl:gap-4">
+        <div className="hidden sm:block col-span-2 row-span-2 shadow-sm hover:shadow-lg rounded-xl lg:rounded-2xl overflow-hidden border dark:border-white/[0.2] dark:bg-[#212121]">
+          <Image
+            src="/bento/ginko.png"
+            alt="sora ginko"
+            width={800}
+            height={800}
+            className="w-full h-full object-cover object-top"
+          />
         </div>
-        <div className="row-span-2 bento-item">Paris: City of Lights</div>
-        <div className="bento-item">Underwater Wonders</div>
-        <div className="col-span-3 bento-item">
-          Trek Through Lush Rainforests
+        <div className="col-span-5 sm:col-span-3 bento-item p-4">
+          <p>
+            Thành thật mà nói thì bạn cần phải có IQ rất cao để hiểu được công
+            dụng thật sự của tiểu thuyết ánh sáng. Tuy bề ngoài chỉ là một cuốn
+            truyện thông thường, nhưng ẩn giấu trong nó là những giá trị tiềm
+            tàng mà không mấy ai tận dụng hết được.
+          </p>
+          <p>
+            Tiểu thuyết ánh sáng có nhiều loại chất liệu và kích thước khác
+            nhau: bìa dày, bìa mỏng, khổ to, khổ nhỏ đều được nhà xuất bản khéo
+            lẹo chọn lựa để phù hợp với bàn tay quý giá của người mua. Đó là
+            chưa nói đến những tấm hình minh họa tuyệt đẹp được những họa sĩ
+            danh tiếng kỳ công vẽ nên, để rồi được nhà xuất bản chăm chút tỉ mỉ
+            in lên trên bìa. Những fan ruột thấu hiểu được điều này; họ sở hữu
+            đủ khả năng trí thức để biết loại nào phù hợp với bàn tay mình, bìa
+            có phải best girl hay không, để nhận ra rằng nó không chỉ ngừng lại
+            ở đó - nó còn có một công dụng đặc biệt khác là dùng để{" "}
+            <strong className="text-red-500 font-black">lót chuột</strong>.
+          </p>
+          <p>
+            Kết quả là một số người mua còn non nớt ở Việt Nam sẽ không nhận ra
+            được công dụng thật sự của nó, để rồi phải bỏ thêm tiền mua những
+            tấm lót chuột tầm thường khác. Chỉ mới nghĩ đến cảnh tượng người ta
+            bỏ hàng đống tiền ra để mua tấm lót chuột của Razor thôi đã làm tôi
+            cười mỏi mồm.
+          </p>
         </div>
-        <div className="bento-item">Canadian Rockies</div>
-        <div className="bento-item">Explore More</div>
+        <div className="col-span-2 sm:col-span-1 bento-item border-none">
+          <Image
+            src="/bento/yue.jpg"
+            alt="yue"
+            width={400}
+            height={400}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="row-span-2 col-span-3 sm:row-span-1 sm:col-span-1 bento-item border-none">
+          <Image
+            src="/bento/aobuta.jpg"
+            alt="aobuta"
+            width={800}
+            height={800}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="col-span-2 row-span-1 sm:col-span-1 sm:row-span-2 bento-item border-none">
+          <Image
+            src="/bento/tanmoshi.jpg"
+            alt="tanmoshi"
+            width={800}
+            height={800}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="col-span-5 sm:col-span-3 row-span-2 bento-item p-6 xl:p-12">
+          <h1 className="text-2xl xl:text-5xl font-black mb-1 sm:mb-4 text-left">
+            LIGHT NOVEL LÀ CÁI CHẾT TỪ TỪ CỦA HÀNH TINH!
+          </h1>
+          <p>
+            Người đọc Light Novel là thảm họa cho nền kinh tế đất nước: họ không
+            mua những tiểu thuyết kinh điển của nhà văn trong nước, họ không cần
+            trả tiền cho những ấn phẩm bìa cứng đắt tiền của các lĩnh vực khoa
+            học khác, họ không cần mua lót chuột, lót ly, tranh treo tường hay
+            những standee đắt tiền, họ không cần đến thư viện, họ không trả tiền
+            cho những hoạt động vui chơi mua sắm bên ngoài mà chỉ ngồi ở nhà đọc
+            truyện, họ không bị cận thị nặng do xem điện thoại quá nhiều, không
+            phải bỏ tiền đi khám mắt, không tốn tiền mua kính và thuốc nhỏ mắt,
+            không bị stress và không cần bác sĩ tâm lý.
+          </p>
+          <p>
+            Ngược lại, mỗi tiệm net mới đều tạo ra ít nhất 10 việc làm: 3 thu
+            ngân, 3 phục vụ, 2 đầu bếp, 2 bảo vệ và tất nhiên là GDP mang lại
+            cho đất nước khi trang bị những PC tối tân hay tai nghe bàn phím đắt
+            tiền.
+          </p>
+          <p>
+            Do đó, hãy lựa chọn thật kĩ:{" "}
+            <strong className="text-red-500 font-black">
+              một độc giả Light Novel
+            </strong>
+            {" hay "}
+            <strong className="text-red-500 font-black">một tiệm net?</strong>
+          </p>
+          <p>Đáng để suy ngẫm.</p>
+          <p>
+            Web novel còn kinh khủng hơn, độc giả web novel thậm chí còn không
+            mua Light Novel.
+          </p>
+        </div>
+        <div className="bento-item border-none col-span-3 sm:col-span-1 row-span-2 sm:row-span-1">
+          <Image
+            src="/bento/spyroom.jpg"
+            alt="spyroom"
+            width={400}
+            height={400}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="col-span-2 bento-item border-none">
+          <Image
+            src="/bento/elaina.jpg"
+            alt="tanmoshi"
+            width={800}
+            height={800}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="col-span-2 sm:col-span-1 bento-item border-none">
+          <Image
+            src="/bento/mobuseka.jpg"
+            alt="mobuseka"
+            width={300}
+            height={400}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="hidden sm:block shadow-sm hover:shadow-lg rounded-xl lg:rounded-2xl overflow-hidden border dark:border-white/[0.2] dark:bg-[#212121]">
+          <Image
+            src="/bento/konosuba.png"
+            alt="konosuba"
+            width={400}
+            height={400}
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        <div className="col-span-5 sm:col-span-3 bento-item p-6">
+          <p>
+            Nói nhỏ cho nghe nhé. Thống kê khoa học đã cho thấy 100% những người
+            đọc Light Novel không phải là tỷ phú được chịch dạo với những cô
+            người mẫu siêu hót, và cũng chẳng làm được gì có ích cho xã hội. Tất
+            cả những đứa đọc Light Novel đều toàn là phế phẩm và cách duy nhất
+            để bọn chúng cảm thấy tự hào về bản thân là đi đọc truyện xàm bô bí
+            đao.
+          </p>
+          <p>
+            Quả là nực cười, đám đó tự lãng phí thời giờ chỉ để có được cảm giác
+            lên đồng nhất thời. Họ làm như mình là người tốt nhất trên cả thế
+            gian khi lập bầy đàn với nhau và cười nhạo những kẻ không biết gì về
+            Light Novel - cái thể loại truyện suy đồi nhất mà nhân loại từng ị
+            ra.
+          </p>
+          <p>
+            Trước khi vào đây cãi lại, nên nhớ tôi chỉ đang nói sự thật, không
+            hề có ý xúc phạm ai cả. Muốn tiếp tục giả vờ như mình không phải
+            hạng người như thế cũng được thôi, muốn nói mấy câu như{" "}
+            <strong className="text-red-500 font-black">
+              “Bọn tôi là bằng hữu đọc LN cùng nhau, bọn tôi thích shitpost,
+              thích homopost, có hại gì ai đâu.”
+            </strong>{" "}
+            cũng được thôi. Nhưng trong thân tâm mấy người chắc chắn đều nhận ra
+            tôi vừa vạch trần bộ mặt thật của tất cả đám đọc Light Novel. Cái
+            thú vui này chỉ có một mục đích là thủ dâm, không hơn, không kém.
+          </p>
+        </div>
       </div>
     </div>
   );
