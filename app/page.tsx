d8ac77b54f25f2b74d0416e02210d70ee93cb964
meokisama <hoangdinhsang123@gmail.com>
2024-08-06 15:06:31 +0700

    change background for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,27 @@ import Bento from "@/components/home/bento";
 import BoosterAds from "@/components/home/booster-ads";
 import Hero from "@/components/home/hero";
 import NewPosts from "@/components/home/new-posts";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <SplashScreen />
       <ModeToggle />
-      <Hero />
-      <Bento />
-      <AuthorSection />
-      <BoosterAds />
-      <NewPosts />
+      <div className="relative">
+        <Image
+          src="/beams.jpg"
+          alt="background image"
+          width={1308}
+          height={1000}
+          className="fixed w-full h-full -z-[100] opacity-40 dark:invert dark:hidden"
+        />
+        <Hero />
+        <Bento />
+        <AuthorSection />
+        <BoosterAds />
+        <NewPosts />
+      </div>
     </main>
   );
 }
--- components/home/author.tsx
+++ components/home/author.tsx
@@ -25,7 +25,7 @@ export default function AuthorSection() {
   ];
   return (
     <>
-      <div className="bg-[hsl(var(--background))] w-full text-center flex flex-col items-center justify-center mt-32">
+      <div className="w-full text-center flex flex-col items-center justify-center mt-32">
         <Separator className="mb-8 max-w-[80%] lg:max-w-4xl" />
         <h2 className="text-4xl md:text-6xl font-black">Tác giả</h2>
         <p className="text-lg lg:text-xl text-center px-4 leading-5 mt-2">
